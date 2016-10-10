cbecce3df5627a3ad61d1f5cc7dc430f6c46be08
sbese <sergbuks2@gmail.com>
2016-10-10 16:47:09 +0300

    1111

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import vk
 from moya import retard
 from saveallphotos import saveallphotomain
 from saveallphotos import savediaphoto
-tokenn="b9264d30e10e88ec6ff2301f049e4cffb35dbbd46be55c952f0506ee4cb6ecf1d8762bd430d3d4da1df24"
+tokenn=retard()
 session = vk.Session(access_token=tokenn)
 api = vk.API(session,v='5.57', lang='ru')
 while 1==1:
